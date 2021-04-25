92033692d141fc2f12559c6b27708a4afec68639
yiquiapaza <yiquiapaza@gmail.com>
2021-04-25 00:42:49 -0300

    Update file

--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-var this_is_string = "Hello world";
-var this_is_number = 12.4;
-var this_is_boolean = false;
-var this_is_any = [];
-console.log(this_is_number, this_is_number, this_is_boolean, this_is_any);
+console.log("Hello world");
