fd8a3a1d8f1805d4f104e5a8e7e7ac49b969f96e
yiquiapaza <yiquiapaza@gmail.com>
2021-04-25 00:43:06 -0300

    Update File

--- test.ts
+++ test.ts
@@ -1,6 +1,1 @@
-let this_is_string: string = "Hello world";
-let this_is_number: number = 12.4;
-let this_is_boolean: boolean = false;
-let this_is_any: any = [];
-console.log(this_is_number, this_is_number, this_is_boolean, this_is_any);
-
+console.log("Hello world");
