11d4196b6475f8b24927b7695fc4ebce908ab55e
Yhonatan Jesus <yiquiapaza@gmail.com>
2021-04-25 00:09:40 -0300

    Add test

---
+++ test.js
@@ -0,0 +1,5 @@
+var this_is_string = "Hello world";
+var this_is_number = 12.4;
+var this_is_boolean = false;
+var this_is_any = [];
+console.log(this_is_number, this_is_number, this_is_boolean, this_is_any);
